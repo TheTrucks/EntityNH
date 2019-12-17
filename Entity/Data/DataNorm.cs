f5a65d6e391e9ce99906e8bdf8a3cbcda3d9df01
TheTrucks <Someone.TraX@gmail.com>
2019-12-17 12:57:43 +1000

    added table data.data_norm

---
+++ Entity/Data/DataNorm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityNH.Entity.Meta;
+using EntityNH.Entity.Parser;
+
+namespace EntityNH.Entity.Data
+{
+    public class DataNorm
+    {
+        public virtual int? Id { get; set; }
+        public virtual Site Site { get; set; }
+        public virtual int? ObservationTime { get; set; }
+        public virtual Variable NormType { get; set; }
+        public virtual double? Value { get; set; }
+    }
+}
---
+++ Mappings/Data/DataNormMap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using EntityNH.Entity.Meta;
+using EntityNH.Entity.Data;
+
+namespace EntityNH.Mappings.Data
+{
+    public class DataNormMap : ClassMap<DataNorm>
+    {
+        public DataNormMap()
+        {
+            Table("data.data_norm");
+            Id(x => x.Id).GeneratedBy.Sequence("data.norma_id_seq");
+            References(x => x.Site, "site_id").Not.Nullable();
+            Map(x => x.ObservationTime, "date_obs").Not.Nullable();
+            References(x => x.NormType, "norm_type_id").Not.Nullable();
+            Map(x => x.Value, "norm_value").Not.Nullable();
+        }
+    }
+}
